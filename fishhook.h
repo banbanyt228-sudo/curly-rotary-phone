1fbfeaed8ef42f09eed801a7e6ec3c095fcb1943
banbanyt228-sudo <banbanyt228@gmail.com>
2026-09-19 12:24:31 +0300

    Create fishhook.h

---
+++ fishhook.h
@@ -0,0 +1,28 @@
+#ifndef FISHHOOK_H
+#define FISHHOOK_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct rebinding {
+  const char *name;
+  void *replacement;
+  void **replaced;
+};
+
+int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel);
+
+int rebind_symbols_image(void *header,
+                         intptr_t slide,
+                         struct rebinding rebindings[],
+                         size_t rebindings_nel);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FISHHOOK_H
